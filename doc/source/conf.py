0e05e16993c9a28d8ecfdf4b11f3e7d46f4a9935
Nikola Petrov <nikolavp@gmail.com>
2014-07-15 11:06:55 +0300

    Better documentation setup
    * Made a local-requirements.txt file that you can use when you are
    building the documentation locally instead of on the read the docs.
    * Call javasphinx process in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,9 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+from subprocess import call
+call(['javasphinx-apidoc', '-f', '-o', 'javadoc', '../../src/main/java/'])
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
