408072136063b09744c249dbf882a06d19119c99
Nikola Petrov <nikolavp@gmail.com>
2014-07-21 15:07:30 +0300

    Use the more modern ProcessBuilder

--- src/main/java/com/nikolavp/approval/reporters/ExecutableDifferenceReporter.java
+++ src/main/java/com/nikolavp/approval/reporters/ExecutableDifferenceReporter.java
@@ -4,8 +4,6 @@ import com.nikolavp.approval.Reporter;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * A reporter that will shell out to an executable that is presented on the user's machine to verify the test output. Note that the approval command and the difference commands can be the same.
@@ -17,7 +15,6 @@ import java.io.OutputStream;
  */
 public class ExecutableDifferenceReporter implements Reporter {
     private final String diffCommand;
-    private final Runtime runtime;
     private final String approvalCommand;
 
     /**
@@ -26,12 +23,6 @@ public class ExecutableDifferenceReporter implements Reporter {
      * @param diffCommand the difference command
      */
     public ExecutableDifferenceReporter(String approvalCommand, String diffCommand) {
-        this(approvalCommand, diffCommand, Runtime.getRuntime());
-    }
-
-
-    ExecutableDifferenceReporter(String approvalCommand, String diffCommand, Runtime runtime) {
-        this.runtime = runtime;
         this.approvalCommand = approvalCommand;
         this.diffCommand = diffCommand;
     }
@@ -39,32 +30,16 @@ public class ExecutableDifferenceReporter implements Reporter {
     @Override
     public void notTheSame(byte[] oldValue, File fileForVerification, byte[] newValue, File fileForApproval) {
         try {
-            Process process = this.runtime.exec(diffCommand + " " + fileForApproval.getAbsolutePath() + " " + fileForVerification.getAbsolutePath());
-            execute(process);
+            execute(diffCommand, fileForApproval.getAbsolutePath(), fileForVerification.getAbsolutePath());
         } catch (IOException e) {
             throw new AssertionError("There was a problem while executing %s", e);
         }
     }
 
-    /* Copy pasted from Files.copy because they don't expose that method. So much fun!*/
-    private static final int BUFFER_SIZE = 8192;
-    private static long copy(InputStream source, OutputStream sink) throws IOException {
-        long nread = 0L;
-        byte[] buf = new byte[BUFFER_SIZE];
-        int n;
-        while ((n = source.read(buf)) > 0) {
-            sink.write(buf, 0, n);
-            nread += n;
-        }
-        return nread;
-    }
-
-
     @Override
     public boolean approveNew(byte[] value, File approvalDestination, File fileForVerification) {
         try {
-            Process process = runtime.exec(approvalCommand + " " + approvalDestination.getAbsolutePath());
-            int processExit = execute(process);
+            int processExit = execute(approvalCommand, approvalDestination.getAbsolutePath());
             if (processExit == 0) {
                 return true;
             }
@@ -75,20 +50,18 @@ public class ExecutableDifferenceReporter implements Reporter {
         return false;
     }
 
-    private int execute(Process process) throws IOException {
-        InputStream inputStream = process.getInputStream();
-        if (inputStream != null) {
-            copy(inputStream, System.out);
-        }
-
-        InputStream errorStream = process.getErrorStream();
-        if (errorStream != null) {
-            copy(errorStream, System.err);
-        }
+    private int execute(String... cmdParts) throws IOException {
+        final Process process = startProcess(cmdParts);
         try {
             return process.waitFor();
         } catch (InterruptedException e) {
             throw new AssertionError("Thread was interrupted while waiting for process to finish", e);
         }
+    }
+
+    Process startProcess(String... cmdParts) throws IOException {
+        return new ProcessBuilder(cmdParts)
+                    .inheritIO()
+                    .start();
     }
 }
