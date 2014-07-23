969fdc96bea6eb78de0d8471293d69d15967fcb2
Nikola Petrov <nikolavp@gmail.com>
2014-07-23 15:38:15 +0300

    Provide abstract classes so we can safely add new methods

---
+++ src/main/java/com/nikolavp/approval/converters/AbstractConverter.java
@@ -0,0 +1,7 @@
+package com.nikolavp.approval.converters;
+
+/**
+ * User: nikolavp (Nikola Petrov) Date: 14-7-23 Time: 15:37
+ */
+public class AbstractConverter {
+}
---
+++ src/main/java/com/nikolavp/approval/reporters/AbstractReporter.java
@@ -0,0 +1,7 @@
+package com.nikolavp.approval.reporters;
+
+/**
+ * User: nikolavp (Nikola Petrov) Date: 14-7-23 Time: 15:36
+ */
+public class AbstractReporter {
+}
