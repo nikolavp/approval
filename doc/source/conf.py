07f4d7476f6ce837edf9f656be2387da8bb2a20c
Nikola Petrov <nikolavp@gmail.com>
2023-07-25 18:00:35 +0300

    Fix readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,26 +16,16 @@ import sys
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-def javadoc_build():
-    javasphinx_command = ['javasphinx-apidoc']
-    if os.environ.get('JAVADOC_FORCE', '0') != '0':
-        javasphinx_command.append('-f')
-    javasphinx_command += ['-o', 'javadoc', '../../src/main/java/']
-    from subprocess import call
-    print(javasphinx_command)
-    call(javasphinx_command)
-
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    javadoc_build()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
@@ -47,7 +37,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # ones.
 extensions = [
     'sphinx.ext.todo',
-    'javasphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
